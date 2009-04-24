c88c9ccbc840c00c04617afc19765ce177bd1a7d
neil.ghosh <neil.ghosh@20227442-29f3-11de-b89a-ab5ced7cf1a0>
2009-04-24 21:09:17 +0000

    getprice method added

--- stockprice/src/ng/StockItem.java
+++ stockprice/src/ng/StockItem.java
@@ -6,31 +6,37 @@ import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
-
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class StockItem {
-    @PrimaryKey
-    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-    private Long id;
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
 
-    @Persistent
-    private String user;
+	@Persistent
+	private String user;
 
-    @Persistent
-    private String stockCode;
+	@Persistent
+	private String stockCode;
 
-    @Persistent
-    private Date date;
+	@Persistent
+	private Date date;
 
-    public StockItem(String user, String stockCode, Date date) {
-    	
-        this.user = user;
-        this.stockCode = stockCode;
-        this.date = date;
-    }
+	public StockItem(String user, String stockCode, Date date) {
 
-    public String getUser() {
+		this.user = user;
+		this.stockCode = stockCode;
+		this.date = date;
+	}
+
+	public String getUser() {
 		return user;
 	}
 
@@ -47,14 +53,35 @@ public class StockItem {
 	}
 
 	public Long getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getStockPrice() {
+		double price = 0.0d;
+		try {
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db
+					.parse("http://testjavaneil.appspot.com/stockprice?q="
+							+ stockCode);
+			doc.getDocumentElement().normalize();
+
+			NodeList nodeLst = doc.getElementsByTagName("price");
+			Element priceElement = (Element) nodeLst.item(0);
+			NodeList prc = priceElement.getChildNodes();
+			price = Double.parseDouble((((Node) prc.item(0)).getNodeValue()));
+		} catch (Exception e) {
+			System.out.print("Ex..");
+		}
+		return price;
+	}
 }
